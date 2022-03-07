c119a718818828fbe296bffd7a5e214cb6b3a330
Darío Rumbo <dario.rumbo@gmail.com>
2022-03-07 00:00:15 -0300

    Formulario

--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,5 @@
 import React from 'react'
 import { render } from 'react-dom'
-
-const App = () => {
-    return (
-        <h1>Hello World</h1>
-    )
-}
+import App from './App'
 
 render(<App />, document.getElementById('app'))
